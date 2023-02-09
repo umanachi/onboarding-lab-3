0793c2cc3c123dbca234152d57e6d913f0026256
umanachi <123425184+umanachi@users.noreply.github.com>
2023-02-08 23:24:36 -0500

    completed lab 3, q1

--- dv/exercise1.cpp
+++ dv/exercise1.cpp
@@ -2,4 +2,31 @@
 
 int main() {
   VExercise1 model;
+  
+  bool failed = false;
+  
+  for (model.op = 0; model.op < 4; model.op ++) {
+    for (model.a = 0; model.a < 256; model.a ++) {
+      for (model.b = 0; model.b < 256; model.b ++) {
+        model.eval();
+        if ((model.op == 0) && ((model.a ^ model.b) != model.out)) {
+          failed = true;
+        }
+        
+        if ((model.op == 1) && ((model.a << model.b) != model.out)) {
+          failed = true;
+        }
+        
+        if ((model.op == 2) && ((model.a % model.b) != model.out)) {
+          failed = true;
+        }
+        
+        if ((model.op == 3) && (~(model.a & model.b) != model.out)) {
+          failed = true;
+        }
+      }
+    }
+  }
+  
+  return failed;
 }
