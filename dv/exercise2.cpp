9792378970c45828cb47b26dd12418b250f5e408
umanachi <123425184+umanachi@users.noreply.github.com>
2023-03-04 13:33:06 -0500

    completed lab 3
    need to test properly

--- dv/exercise2.cpp
+++ dv/exercise2.cpp
@@ -1,5 +1,47 @@
 #include <VExercise2.h>
 
-int main() {
+// calculate expected shifted output
+uint16_t lfsr(uint16_t curr) {
+  return (curr << 1) | ( ((curr >> 10) & 1) ^ ((curr >> 8) & 1) ^ ((curr >> 3) & 1) ^ ((curr >> 1) & 1));
+}
+
+bool reading_rainbow(uint16_t input) {
   VExercise2 model;
+  
+  bool failed = false;
+  
+  // initialize module & check reset output
+  model.clk = 1;
+  model.eval();
+  model.clk = 0;
+  model.reset = 1;
+  model.init = input;
+  model.eval();
+  if (model.out != ~input) {
+    failed = true;
+  }
+  
+  uint16_t previous = ~input;
+  for (int i = 0; i < 10000; i++) {
+    model.clk = 1;
+    model.reset = 0;
+    model.eval();
+    model.clk = 0;
+    model.reset = 0;
+    model.eval();
+    if (model.out != lfsr(prev)) {
+      failed = true;
+    }
+    previous = model.out;
+  }
+  
+  return failed;
+}
+
+int main() {
+  bool master_fail = false;
+  for (int i = 0; i < 10; i++) {
+    uint16_t test_val = rand() % 65535;
+    master_fail = reading_rainbow(test_val);
+    return failed;
 }
